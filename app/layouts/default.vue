c9ff1da9491e16b9bbf79ac43248b3b94c1d002f
ruanvalente <ruanopensuselinux@gmail.com>
2026-06-23 11:54:37 -0300

    feat: initialize Nuxt 4 project with TypeScript, Tailwind CSS, and Google Fonts
    - Added nuxt.config.ts for project configuration
    - Created package.json with necessary scripts and dependencies
    - Added favicon.ico to public directory
    - Created robots.txt for web crawling instructions
    - Added tsconfig.json for TypeScript support

---
+++ app/layouts/default.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+useHead({
+  title: "ContaAI - Descubra Autores e Suas Contribuições Literárias",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Plataforma de compartilhamento de contos, histórias e muito mais. De forma simples e gratuita.",
+    },
+  ],
+  htmlAttrs: {
+    lang: "pt-BR",
+    "data-scroll-behavior": "smooth",
+  },
+});
+</script>
+
+<template>
+  <main class="min-h-screen flex flex-col">
+    <slot />
+
+    <!-- TODO: NotificationManagementWidget -->
+  </main>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white"
+  >
+    <h2>Opa</h2>
+  </div>
+</template>
